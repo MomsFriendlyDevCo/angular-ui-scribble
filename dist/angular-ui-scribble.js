90f3690bae44bfd6d135424621ea6e7d9645826e
Matt Carter <m@ttcarter.com>
2017-04-05 15:44:35 +1000

    Compile Angular files + Publish

--- dist/angular-ui-scribble.js
+++ dist/angular-ui-scribble.js
@@ -26,120 +26,99 @@ angular.module('angular-ui-scribble', []).factory('$debounce', ['$timeout', func
 }]).directive('uiScribble', function () {
 	return {
 		scope: {
-			config: '=?',
-			callbackFn: '&',
-			callbackBtn: '&',
-			buttons: '=?'
+			callback: '&',
+			editable: '<',
+			buttons: '<',
+			width: '@?',
+			height: '@?'
 		},
-		template: '\n\t\t\t<div class="scribble">\n\t\t\t\t<nav class="scribble-actions navbar navbar-default" style="width: {{scribbleWidth}}px">\n\t\t\t\t\t<div class="container-fluid">\n\t\t\t\t\t\t<ul class="nav navbar-nav">\n\t\t\t\t\t\t\t<li><a ng-click="clearSignature()" class="btn btn-danger"><i class="fa fa-trash"></i></a></li>\n\t\t\t\t\t\t\t<li ng-if="mode!=\'erase\'"><a ng-click="setMode(\'erase\')" tooltip="Eraser" class="btn btn-default"><i class="fa fa-eraser"></i></a></li>\n\t\t\t\t\t\t\t<li ng-if="mode==\'erase\'"><a ng-click="setMode(\'pen\')" tooltip="Pen" class="btn btn-default"><i class="fa fa-pencil"></i></a></li>\n\t\t\t\t\t\t\t<li ng-if="buttons.camera && mode!=\'streaming\' && !isMobile" tooltip="Set background image"><a ng-click="setBackground()" class="btn btn-default"><i class="fa fa-image"></i></a></li>\n\t\t\t\t\t\t\t<li ng-if="buttons.camera && mode==\'streaming\' && !isMobile" tooltip="Take screenshot"><a ng-click="screenshot()" class="btn btn-default"><i class="fa fa-camera"></i></a></li>\n\t\t\t\t\t\t\t<li ng-show="buttons.camera && isMobile">\n\t\t\t\t\t\t\t\t<input class="scrible-file-camera selectBackground" type="file" accept="image/*" capture="camera">\n\t\t\t\t\t\t\t\t<a ng-click="requestCamera()" class="btn btn-default"><i class="fa fa-camera"></i></a>\n\t\t\t\t\t\t\t</li>\n\t\t\t\t\t\t</ul>\n\t\t\t\t\t</div>\n\t\t\t\t</nav>\n\t\t\t\t<div class="scribble-area" style="width: {{scribbleWidth}}px; height: {{scribbleWidth}}px">\n\t\t\t\t\t<canvas class="scribble-board" height="{{scribbleHeight}}" width="{{scribbleWidth}}"></canvas>\n\t\t\t\t\t<video class="scribble-video" ng-show="mode==\'streaming\'" height="{{scribbleHeight}}" width="{{scribbleWidth}}" autoplay></video>\n\t\t\t\t\t<canvas class="scribble-background" ng-show="mode!=\'streaming\'" height="{{scribbleHeight}}" width="{{scribbleWidth}}"></canvas>\n\t\t\t\t\t<a ng-if="signatureReady" ng-click="callbackBtn({signature: getSignatureImage()})" class="btn btn-success btn-circular btn-fab"><i class="fa fa-fw fa-check fa-2x"></i></a>\n\t\t\t\t</div>\n\t\t\t\t<canvas class="scribble-composed" ng-show=false height="{{scribbleHeight}}" width="{{scribbleWidth}}" ></canvas>\n\t\t\t</div>\n\t\t',
-		controller: ["$scope", "$element", "$attrs", "$debounce", function controller($scope, $element, $attrs, $debounce) {
+		template: '\n\t\t\t<div class="scribble" ng-class="editable ? \'scribble-editable\' : \'scribble-not-editable\'">\n\t\t\t\t<input class="scribble-file-camera selectBackground" type="file" accept="image/*" capture="camera">\n\t\t\t\t<nav ng-if="editable" class="scribble-actions navbar navbar-default" style="width: {{width}}px">\n\t\t\t\t\t<div class="container-fluid">\n\t\t\t\t\t\t<ul class="nav navbar-nav">\n\t\t\t\t\t\t\t<li><a ng-click="clearSignature()" class="btn btn-danger"><i class="fa fa-trash"></i></a></li>\n\t\t\t\t\t\t\t<li ng-if="mode!=\'erase\'"><a ng-click="setMode(\'erase\')" tooltip="Eraser" class="btn btn-default"><i class="fa fa-eraser"></i></a></li>\n\t\t\t\t\t\t\t<li ng-if="mode==\'erase\'"><a ng-click="setMode(\'pen\')" tooltip="Pen" class="btn btn-default"><i class="fa fa-pencil"></i></a></li>\n\t\t\t\t\t\t\t<li ng-if="buttons.camera && mode!=\'streaming\' && !isMobile" tooltip="Set background image"><a ng-click="setBackground()" class="btn btn-default"><i class="fa fa-image"></i></a></li>\n\t\t\t\t\t\t\t<li ng-if="buttons.camera && mode==\'streaming\' && !isMobile" tooltip="Take screenshot"><a ng-click="screenshot()" class="btn btn-default"><i class="fa fa-camera"></i></a></li>\n\t\t\t\t\t\t\t<li ng-show="buttons.camera && isMobile">\n\t\t\t\t\t\t\t\t<a ng-click="requestCamera()" class="btn btn-default"><i class="fa fa-camera"></i></a>\n\t\t\t\t\t\t\t</li>\n\t\t\t\t\t\t</ul>\n\t\t\t\t\t</div>\n\t\t\t\t</nav>\n\t\t\t\t<div class="scribble-area" style="width: {{width}}px; height: {{height}}px">\n\t\t\t\t\t<canvas class="scribble-board" height="{{height}}" width="{{width}}"></canvas>\n\t\t\t\t\t<video class="scribble-video" ng-show="mode==\'streaming\'" height="{{height}}" width="{{width}}" autoplay></video>\n\t\t\t\t\t<canvas class="scribble-background" ng-show="mode!=\'streaming\'" height="{{height}}" width="{{width}}"></canvas>\n\t\t\t\t\t<a ng-if="signatureReady" ng-click="submit()" class="btn btn-success btn-circular btn-fab"><i class="fa fa-fw fa-check fa-2x"></i></a>\n\t\t\t\t</div>\n\t\t\t\t<canvas class="scribble-composed" height="{{height}}" width="{{width}}"></canvas>\n\t\t\t</div>\n\t\t',
+		controller: ["$scope", "$element", "$debounce", function controller($scope, $element, $debounce) {
 			// Mobile version {{{
 			var userAgent = navigator.userAgent;
 			$scope.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(userAgent);
 			$scope.requestCamera = function () {
-				$element.find('input[type=file]').trigger('click');
+				return $element.find('input[type=file]').trigger('click');
 			};
 			// }}}
-			$scope.mode = 'pen';
-			$scope.signaturePad;
-			$scope.scribbleHeight = 400;
-			$scope.scribbleWidth = 400;
-			$scope.buttons = $scope.buttons || { camera: true };
 
+			// Deal with user config {{{
+			if (!$scope.height) $scope.height = 400;
+			if (!$scope.width) $scope.width = 400;
+
+			$scope.buttons = Object.assign({ // Set default buttons unless overriden
+				camera: true
+			}, $scope.buttons);
+			// }}}
+
+			// Screenshot management {{{
 			var canvas = $element[0].querySelector('.scribble-board');
 			var ctx = canvas.getContext('2d');
+			$scope.signaturePad = new SignaturePad(canvas);
 
 			var canvasBackground = $element[0].querySelector('.scribble-background');
 			var ctxBackground = canvasBackground.getContext('2d');
 			var composedImage = $element[0].querySelector('.scribble-composed');
 			var ctxComposed = composedImage.getContext('2d');
-
-			// Flip the screenshot {{{
-			//TODO: not flipping the screenshot
-			var reversed = false;
-			// ctxBackground.translate(canvasBackground.width, 0);
-			// ctxBackground.scale(-1, 1);
-			// }}}
-
-			$scope.signaturePad = new SignaturePad(canvas);
-
 			var video = $element[0].querySelector('.scribble-video');
 			var videoStream;
 			// check for getUserMedia support
 			navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
 			$scope.setBackground = function () {
-				// start video feed
-				$scope.setMode('streaming');
-
-				// get webcam feed if available
-				if (navigator.getUserMedia) navigator.getUserMedia({ video: true }, handleVideo, videoError);
+				$scope.setMode('streaming'); // Start video feed
+				if (navigator.getUserMedia) navigator.getUserMedia({ video: true }, function (stream) {
+					// Get webcam feed if available
+					video.src = window.URL.createObjectURL(stream);
+					videoStream = stream;
+				}, function () {});
 			};
 
-			function handleVideo(stream) {
-				video.src = window.URL.createObjectURL(stream);
-				videoStream = stream;
-			}
-
-			function videoError() {}
-
+			$scope.reversed = false;
 			$scope.screenshot = function () {
 				$scope.setMode('pen');
 				if (video.paused || video.ended) console.log("no video");;
 				if (video.paused || video.ended) return false;
 				//TODO: hack to flip context only once {{{
-				if (!reversed) {
-					reversed = true;
+				if (!$scope.reversed) {
+					$scope.reversed = true;
 					ctxBackground.translate(canvasBackground.width, 0);
 					ctxBackground.scale(-1, 1);
 				}
 				// }}}
 
-				ctxBackground.drawImage(video, 0, 0, $scope.scribbleWidth, $scope.scribbleHeight);
+				ctxBackground.drawImage(video, 0, 0, $scope.width, $scope.height);
 				videoStream.getTracks()[0].stop();
 			};
+			// }}}
 
-			// Expose original signaturePad object
-			$scope.config.getSignaturePad = function () {
-				return signaturePad;
-			};
 
-			// Expose composed image
-			$scope.config.getSignatureImage = $scope.getSignatureImage;
-
-			// Returns composed image of background and foreground
-			$scope.getSignatureImage = function () {
-				ctxComposed.clearRect(0, 0, composedImage.width, composedImage.height);
-				ctxComposed.drawImage(canvasBackground, 0, 0);
-				ctxComposed.drawImage(canvas, 0, 0);
-				return composedImage.toDataURL();
-			};
-
+			// Handle signature pad events {{{
 			$scope.clearSignature = function () {
-				$scope.signaturePad.clear();
+				return $scope.signaturePad.clear();
 			};
 
-			$scope.signaturePad.onBegin = function (e) {
-				$scope.$applyAsync(function () {
-					$scope.signatureReady = false;
+			$scope.signaturePad.onBegin = function () {
+				return $scope.$applyAsync(function () {
+					return $scope.signatureReady = false;
 				});
 			};
 
-			function signatureReady() {
-				$scope.$applyAsync(function () {
-					if ($attrs.callbackFn && typeof $scope.callbackFn === 'function') {
-						var image = $scope.getSignatureImage();
-						$scope.callbackFn({ signature: image });
-					} else if ($attrs.callbackBtn && typeof $scope.callbackBtn === 'function') {
+			$scope.signaturePad.onEnd = $debounce(function () {
+				return $scope.$applyAsync(function () {
+					if ($scope.editable) {
 						$scope.signatureReady = true;
+					} else {
+						$scope.submit();
 					}
 				});
-			}
+			}, 1500, false);
+			// }}}
 
-			$scope.signaturePad.onEnd = $debounce(signatureReady, 1500, false);
-
+			// Manage mode {{{
+			$scope.mode = 'pen';
 			$scope.setMode = function (mode) {
-				$scope.mode = mode;
+				return $scope.mode = mode;
 			};
 
 			$scope.$watch('mode', function (newVal, oldVal) {
@@ -158,32 +137,41 @@ angular.module('angular-ui-scribble', []).factory('$debounce', ['$timeout', func
 					$scope.signaturePad.maxWidth = $scope.oldStroke.maxWidth;
 				}
 			});
+			// }}}
 
 			// Background - mobile {{{
 			var selectBackground = $element[0].querySelector('.selectBackground');
-			var reader;
-
 			selectBackground.addEventListener('change', function (e) {
 				var backgroundSrc = selectBackground.files[0];
-				reader = new FileReader();
+				var reader = new FileReader();
 
 				reader.onload = function (event) {
-					loadBackground(event.target.result);
+					var image = new Image();
+					var ratio = window.devicePixelRatio || 1;
+
+					image.src = event.target.result;
+					image.onload = function () {
+						ctxBackground.clearRect(0, 0, canvas.width, canvas.height);
+						ctxBackground.drawImage(image, 0, 0, canvasBackground.width, canvasBackground.height);
+					};
 				};
 
 				reader.readAsDataURL(backgroundSrc);
 			});
+			// }}}
 
-			function loadBackground(dataUrl) {
-				var image = new Image();
-				var ratio = window.devicePixelRatio || 1;
+			// Submit signature {{{
+			$scope.getSignatureImage = function () {
+				ctxComposed.clearRect(0, 0, composedImage.width, composedImage.height);
+				ctxComposed.drawImage(canvasBackground, 0, 0);
+				ctxComposed.drawImage(canvas, 0, 0);
+				return composedImage.toDataURL();
+			};
 
-				image.src = dataUrl;
-				image.onload = function () {
-					ctxBackground.clearRect(0, 0, canvas.width, canvas.height);
-					ctxBackground.drawImage(image, 0, 0, canvasBackground.width, canvasBackground.height);
-				};
-			}
+			$scope.submit = function () {
+				var image = $scope.getSignatureImage();
+				$scope.callback({ image: image });
+			};
 			// }}}
 		}]
 	};
