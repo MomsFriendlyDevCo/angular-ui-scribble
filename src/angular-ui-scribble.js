0a42ca6934c5f0daceb2292c0677ed58e01dff09
Matt Carter <m@ttcarter.com>
2017-04-05 15:14:01 +1000

    Code tidy-up

--- src/angular-ui-scribble.js
+++ src/angular-ui-scribble.js
@@ -26,14 +26,13 @@ angular.module('angular-ui-scribble',[])
 .directive('uiScribble', function(){
 	return {
 		scope:{
-			config:'=?',
-			callbackFn: '&',
-			callbackBtn: '&',
-			buttons:'=?'
+			callback: '&',
+			editable: '<',
+			buttons:'<',
 		},
 		template: `
 			<div class="scribble">
-				<nav class="scribble-actions navbar navbar-default" style="width: {{scribbleWidth}}px">
+				<nav class="scribble-actions navbar navbar-default" style="width: {{width}}px">
 					<div class="container-fluid">
 						<ul class="nav navbar-nav">
 							<li><a ng-click="clearSignature()" class="btn btn-danger"><i class="fa fa-trash"></i></a></li>
@@ -48,124 +47,89 @@ angular.module('angular-ui-scribble',[])
 						</ul>
 					</div>
 				</nav>
-				<div class="scribble-area" style="width: {{scribbleWidth}}px; height: {{scribbleWidth}}px">
-					<canvas class="scribble-board" height="{{scribbleHeight}}" width="{{scribbleWidth}}"></canvas>
-					<video class="scribble-video" ng-show="mode=='streaming'" height="{{scribbleHeight}}" width="{{scribbleWidth}}" autoplay></video>
-					<canvas class="scribble-background" ng-show="mode!='streaming'" height="{{scribbleHeight}}" width="{{scribbleWidth}}"></canvas>
-					<a ng-if="signatureReady" ng-click="callbackBtn({signature: getSignatureImage()})" class="btn btn-success btn-circular btn-fab"><i class="fa fa-fw fa-check fa-2x"></i></a>
+				<div class="scribble-area" style="width: {{width}}px; height: {{width}}px">
+					<canvas class="scribble-board" height="{{height}}" width="{{width}}"></canvas>
+					<video class="scribble-video" ng-show="mode=='streaming'" height="{{height}}" width="{{width}}" autoplay></video>
+					<canvas class="scribble-background" ng-show="mode!='streaming'" height="{{height}}" width="{{width}}"></canvas>
+					<a ng-if="signatureReady" ng-click="submit()" class="btn btn-success btn-circular btn-fab"><i class="fa fa-fw fa-check fa-2x"></i></a>
 				</div>
-				<canvas class="scribble-composed" ng-show=false height="{{scribbleHeight}}" width="{{scribbleWidth}}" ></canvas>
+				<canvas class="scribble-composed" ng-show=false height="{{height}}" width="{{width}}" ></canvas>
 			</div>
 		`,
-		controller: function($scope, $element, $attrs, $debounce){
+		controller: function($scope, $element, $debounce){
 			// Mobile version {{{
 			var userAgent = navigator.userAgent;
 			$scope.isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(userAgent));
-			$scope.requestCamera = function(){
-				$element.find('input[type=file]').trigger('click');
-			};
+			$scope.requestCamera = ()=> $element.find('input[type=file]').trigger('click');
 			// }}}
-			$scope.mode = 'pen';
-			$scope.signaturePad;
-			$scope.scribbleHeight = 400;
-			$scope.scribbleWidth = 400;
-			$scope.buttons = $scope.buttons || { camera: true };
 
+			// Deal with user config {{{
+			if (!$scope.height) $scope.height = 400;
+			if (!$scope.width) $scope.width = 400;
+
+			$scope.buttons = Object.assign({ // Set default buttons unless overriden
+				camera: true,
+			}, $scope.buttons);
+			// }}}
+
+			// Screenshot management {{{
 			var canvas = $element[0].querySelector('.scribble-board');
 			var ctx = canvas.getContext('2d');
+			$scope.signaturePad = new SignaturePad(canvas);
 
 			var canvasBackground = $element[0].querySelector('.scribble-background');
 			var ctxBackground = canvasBackground.getContext('2d');
 			var composedImage = $element[0].querySelector('.scribble-composed');
 			var ctxComposed = composedImage.getContext('2d');
-
-			// Flip the screenshot {{{
-			//TODO: not flipping the screenshot
-			var reversed = false;
-			// ctxBackground.translate(canvasBackground.width, 0);
-			// ctxBackground.scale(-1, 1);
-			// }}}
-
-			$scope.signaturePad = new SignaturePad(canvas);
-
 			var video = $element[0].querySelector('.scribble-video');
 			var videoStream;
 			// check for getUserMedia support
 			navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
 			$scope.setBackground = function(){
-				// start video feed
-				$scope.setMode('streaming');
-
-				// get webcam feed if available
-				if(navigator.getUserMedia) navigator.getUserMedia({video: true}, handleVideo, videoError);
+				$scope.setMode('streaming'); // Start video feed
+				if (navigator.getUserMedia) navigator.getUserMedia({video: true}, stream => { // Get webcam feed if available
+					video.src = window.URL.createObjectURL(stream);
+					videoStream = stream;
+				}, function() {});
 			};
 
-			function handleVideo(stream){
-				video.src = window.URL.createObjectURL(stream);
-				videoStream = stream
-			}
-
-			function videoError(){}
-
+			$scope.reversed = false;
 			$scope.screenshot = function(){
 				$scope.setMode('pen');
 				if(video.paused || video.ended) console.log("no video");;
 				if(video.paused || video.ended) return false;
 				//TODO: hack to flip context only once {{{
-				if (!reversed) {
-					reversed = true;
+				if (!$scope.reversed) {
+					$scope.reversed = true;
 					ctxBackground.translate(canvasBackground.width, 0);
 					ctxBackground.scale(-1, 1);
 				}
 				// }}}
 
-				ctxBackground.drawImage(video, 0, 0, $scope.scribbleWidth, $scope.scribbleHeight);
+				ctxBackground.drawImage(video, 0, 0, $scope.width, $scope.height);
 				videoStream.getTracks()[0].stop();
 			};
+			// }}}
 
-			// Expose original signaturePad object
-			$scope.config.getSignaturePad = function(){
-				return signaturePad;
-			};
 
-			// Expose composed image
-			$scope.config.getSignatureImage = $scope.getSignatureImage;
+			// Handle signature pad events {{{
+			$scope.clearSignature = ()=> $scope.signaturePad.clear();
 
-			// Returns composed image of background and foreground
-			$scope.getSignatureImage = function(){
-				ctxComposed.clearRect(0, 0, composedImage.width, composedImage.height);
-				ctxComposed.drawImage(canvasBackground, 0, 0);
-				ctxComposed.drawImage(canvas, 0, 0);
-				return composedImage.toDataURL();
-			};
+			$scope.signaturePad.onBegin = ()=> $scope.$applyAsync(()=> $scope.signatureReady = false);
 
-			$scope.clearSignature = function(){
-				$scope.signaturePad.clear();
-			};
+			$scope.signaturePad.onEnd = $debounce(()=> $scope.$applyAsync(()=> {
+				if ($scope.editable) {
+					$scope.signatureReady = true;
+				} else {
+					$scope.submit();
+				}
+			}), 1500, false);
+			// }}}
 
-			$scope.signaturePad.onBegin = function(e){
-				$scope.$applyAsync(function(){
-					$scope.signatureReady = false;
-				});
-			};
-
-			function signatureReady(){
-				$scope.$applyAsync(function(){
-					if ($attrs.callbackFn && typeof $scope.callbackFn === 'function') {
-						var image = $scope.getSignatureImage();
-						$scope.callbackFn({ signature: image });
-					} else if($attrs.callbackBtn && typeof $scope.callbackBtn === 'function') {
-						$scope.signatureReady = true;
-					}
-				});
-			}
-
-			$scope.signaturePad.onEnd = $debounce(signatureReady, 1500, false);
-
-			$scope.setMode = function(mode){
-				$scope.mode = mode;
-			};
+			// Manage mode {{{
+			$scope.mode = 'pen';
+			$scope.setMode = mode => $scope.mode = mode;
 
 			$scope.$watch('mode', function(newVal, oldVal){
 				if (newVal == 'erase' && newVal !== oldVal) {
@@ -183,32 +147,41 @@ angular.module('angular-ui-scribble',[])
 					$scope.signaturePad.maxWidth = $scope.oldStroke.maxWidth;
 				}
 			});
+			// }}}
 
 			// Background - mobile {{{
 			var selectBackground = $element[0].querySelector('.selectBackground')
-			var reader;
-
 			selectBackground.addEventListener('change', function(e){
 				var backgroundSrc = selectBackground.files[0];
-				reader = new FileReader();
+				var reader = new FileReader();
 
 				reader.onload = function(event){
-					loadBackground(event.target.result);
+					var image = new Image();
+					var ratio = window.devicePixelRatio || 1;
+
+					image.src = event.target.result;
+					image.onload = function () {
+						ctxBackground.clearRect(0, 0, canvas.width, canvas.height);
+						ctxBackground.drawImage(image, 0, 0, canvasBackground.width, canvasBackground.height);
+					};
 				};
 
 				reader.readAsDataURL(backgroundSrc);
 			});
+			// }}}
 
-			function loadBackground(dataUrl){
-				var image = new Image();
-				var ratio = window.devicePixelRatio || 1;
+			// Submit signature {{{
+			$scope.getSignatureImage = function(){
+				ctxComposed.clearRect(0, 0, composedImage.width, composedImage.height);
+				ctxComposed.drawImage(canvasBackground, 0, 0);
+				ctxComposed.drawImage(canvas, 0, 0);
+				return composedImage.toDataURL();
+			};
 
-				image.src = dataUrl;
-				image.onload = function () {
-					ctxBackground.clearRect(0, 0, canvas.width, canvas.height);
-					ctxBackground.drawImage(image, 0, 0, canvasBackground.width, canvasBackground.height);
-				};
-			}
+			$scope.submit = ()=> {
+				var image = $scope.getSignatureImage();
+				$scope.callback({image});
+			};
 			// }}}
 		}
 	}
